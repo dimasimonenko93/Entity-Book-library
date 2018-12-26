1fc73a096f4d0bc6a4c210b2fde0d0b2e1a3b5c3
Dima <dimasimonenko93@gmail.com>
2018-12-26 13:44:06 +0200

    Book and BookContext classes was added. Entity Framework was used for creating database.

---
+++ BookLibrary/BookLibrary/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    class Book
+    {
+        public int    Id              { get; set; }
+        public string Name            { get; set; }
+        public string Writers         { get; set; }
+        public int    PublicationYear { get; set; }
+        public string Genre           { get; set; }
+        public string Language        { get; set; }
+        public int    NumberOfPages   { get; set; }
+        public string Publisher       { get; set; }
+        public int    RecommendedAge  { get; set; }
+        public bool   IsHere          { get; set; }
+        public long   TimeToReturn    { get; set; }
+    }
+}
---
+++ BookLibrary/BookLibrary/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookLibrary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (BookContext db = new BookContext())
+            {
+                Book book1 = new Book { Name = "Mathematics" };
+                Book book2 = new Book { Name = "Geography" };
+
+                db.Books.Add(book1);
+                db.Books.Add(book2);
+                db.SaveChanges();
+            }
+        }
+    }
+}
